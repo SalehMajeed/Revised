27c47caf03dd2aaa134c6c5aa2540fb8967f1166
SalehMajeed <saleh.majeed8@gmail.com>
2020-09-06 16:37:02 -0400

    6

---
+++ js-recent-parts/Table of Contents.js
@@ -0,0 +1,12 @@
+/**
+* 1-> js-recent-parts  
+* 
+* // Kyle Simpson ->
+*  Strings
+*  Array Destructuring
+*  Object Destructuring
+*  Furter Destructuring
+*  Array Methods
+*  Iterators & Generators
+*  Regular Expressions
+*/
