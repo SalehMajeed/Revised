f8f2817bd34c051e99295b79105660053087d80b
SalehMajeed <saleh.majeed8@gmail.com>
2020-09-07 17:54:48 -0400

    js

---
+++ javascript-hard-parts-v2/Table of Contents.js
@@ -0,0 +1,12 @@
+/**
+* 1-> js-recent-parts  
+* 
+* // Will Sentance ->
+*  JavaScript Principles
+*  Functions & Callbacks
+*  Closure
+*  Asynchronous JavaScript
+*  Promises
+*  Classes & Prototypes
+*  Regular Expressions
+*/
