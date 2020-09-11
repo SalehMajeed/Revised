ada9831d4e05b814f155c6c970112109d9c155d0
SalehMajeed <saleh.majeed8@gmail.com>
2020-09-11 15:02:11 -0400

    OOJ

---
+++ object-oriented-js/Table of Contents.js
@@ -0,0 +1,13 @@
+
+/**
+ * 1-> object-oriented-js
+ * 
+ * // Will Sentance ->
+ *      Object Creation
+ *      Prototype & new
+ *      Scope & this
+ *      Default Prototype Chain
+ *      Subclassing with Factory Functions
+ *      Subclassing with new and call
+ *      Subclassing with class, extends & super
+ */
